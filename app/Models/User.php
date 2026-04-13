9d52327be151de4c97ced9925d968482603732d5
KevinDaCruz <kevindc39@hotmail.com>
2026-04-13 14:54:20 +0200

    Activité 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
@@ -34,5 +35,10 @@ class User extends Authenticatable
     public function settings(): HasOne
     {
         return $this->hasOne(Settings::class);
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
     }
 }
---
+++ database/seeders/OrderSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::where('email', 'test@example.com')->first();
+
+        if ($user) {
+            $user->orders()->create([
+                'amount' => 50,
+                'status' => 'paid',
+            ]);
+        }
+    }
+}
